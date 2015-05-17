1bf2a3d6729db5536eac0f6eddab118e708eca88
Otavio Carneiro <otavio@carneiro.blog.br>
2015-05-17 20:55:46 -0300

    Identifica a duração do arquivo

--- video_desc.py
+++ video_desc.py
@@ -1,27 +1,16 @@
 import os, subprocess
 
 def getLength(filename):
-  #result = subprocess.Popen(["ffprobe", filename],stdout = subprocess.PIPE, stderr = subprocess.STDOUT).communicate()[0].decode('UTF-8')
-  result = subprocess.Popen(["ffprobe", filename],stdout = subprocess.PIPE, stderr = subprocess.STDOUT).communicate()
-  if result[0]:
-      print('----------- ZERO -------------')
-      print (result[0].decode('UTF-8'))
-  if result[1]:
-      print('-----------  UM  -------------')
-      print (result[1].decode('UTF-8'))
-  #print(type(result))
-  #print(type(result.stdout.readlines()))
-  #for resultline in result.stdout.readlines():
-    #  if 'Duration' in resultline:
-    #      print (resultline)
-  #return [x for x in result.stdout.readlines() if "Duration" in x]
+  result = subprocess.Popen(["ffprobe", filename],stdout = subprocess.PIPE, stderr = subprocess.STDOUT).communicate()[0].decode('UTF-8')
+  for resultline in result.rsplit(sep="\n"):  #quebra o resultado em linhas
+        if 'Duration' in resultline:
+            print (resultline)
 
-print (os.getcwd())
+print ("Lendo diretório: ", os.getcwd())
 for filename in os.listdir():
-    print (filename)
-    ext = filename[-3:]
-    if ext == 'MOV' or ext == 'MP4':
-        print ('vídeo!')
+    print (" -- Lendo arquivo: ", filename)
+    ext = filename[-3:] #extensão do arquivo
+    if ext == 'MOV' or ext == 'MP4': #se for um vídeo...
         getLength(filename)
 
 
